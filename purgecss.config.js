f2eae225c7d1916961d8b0d0e27ab2802b22232b
kzoeps <kzoepa@gmail.com>
2024-09-20 22:47:00 +0600

    purge owl carousel and remove from conference

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     content: ["*.html"],
-    css: ["css/bootstrap.min.css", "css/jquery-ui.css", "css/style.css"],
+    css: [
+        "css/bootstrap.min.css",
+        "css/jquery-ui.css",
+        "css/style.css",
+        "css/owl.carousel.min.css",
+    ],
     output: "build/css",
 }
